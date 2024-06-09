82e35da8d20da352416653f127695c25cfc06a5d
NehaChawdipande <nehachawdipande10@gmail.com>
2024-06-09 15:59:20 +0530

    updated logic

--- portfoliov2/src/App.tsx
+++ portfoliov2/src/App.tsx
@@ -1,16 +1,20 @@
 import "./App.scss";
-
 function App() {
+  const embed  = () =>{
+    return {
+      __html : '<spline-viewer loading-anim-type="spinner-big-light" url="https://prod.spline.design/oQF2wFKmvwQjdZjp/scene.splinecode"></spline-viewer>'
+    }
+  }
   return (
     <>
         <div className="App">
-        <iframe className="laptopView" title="laptopView" src='https://my.spline.design/portfoliolandscapecopy-9c51313ccd2976e0f1764142768a8f5d/' frameBorder='0' width='100vw' height='100vh' style={{
+    <div className="mobileView" dangerouslySetInnerHTML={embed()} style={{
+                 width: "99vw",
+                 height: "99vh" }}>
+            </div>
+         <iframe className="laptopView" title="laptopView" src='https://my.spline.design/portfoliolandscapecopy-9c51313ccd2976e0f1764142768a8f5d/' frameBorder='0' width='100vw' height='100vh' style={{
                  width: "99vw",
                  height: "99vh" }} ></iframe>
-
-<iframe className="mobileView" title="mobileView" src='https://my.spline.design/friendskawaiicopycopy-bbbe837d783b205174ec965046e4a2b0/' frameBorder='0' width='100%' height='100%' style={{
-                 width: "99vw",
-                 height: "99vh" }}></iframe>
 
           {/* <ComingSoon /> */}
          

bff353885876de70d1e38fa422bdaa166ea7a99f
NehaChawdipande <nehachawdipande10@gmail.com>
2024-06-09 17:05:04 +0530

    embed added

--- portfoliov2/src/App.tsx
+++ portfoliov2/src/App.tsx
@@ -5,6 +5,11 @@ function App() {
       __html: '<spline-viewer loading-anim-type="spinner-big-light" url="https://prod.spline.design/oQF2wFKmvwQjdZjp/scene.splinecode"></spline-viewer>'
     }
   }
+  const embedLargeView = () => {
+    return {
+      __html: '<spline-viewer loading-anim-type="spinner-big-light" url="https://prod.spline.design/d54UYJ7lfP9yBrNO/scene.splinecode"></spline-viewer>'
+    }
+  }
   return (
     <>
       <div className="App">
@@ -13,10 +18,11 @@ function App() {
           height: "99vh"
         }}>
         </div>
-        <iframe className="laptopView" title="laptopView" src='https://my.spline.design/portfoliolandscapecopy-9c51313ccd2976e0f1764142768a8f5d/' frameBorder='0' width='100vw' height='100vh' style={{
-                 width: "99vw",
-                 height: "99vh" }} ></iframe>
-
+        <div className="laptopView" dangerouslySetInnerHTML={embedLargeView()} style={{
+          width: "99vw",
+          height: "99vh"
+        }}>
+        </div>
         {/* <ComingSoon /> */}
 
       </div>

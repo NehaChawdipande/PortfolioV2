93b94dc6043a7901f1be5ecd458d5223f75deffb
NehaChawdipande <nehachawdipande10@gmail.com>
2024-06-09 16:41:27 +0530

    temporarily removed laptopview

--- portfoliov2/src/App.tsx
+++ portfoliov2/src/App.tsx
@@ -1,24 +1,25 @@
 import "./App.scss";
 function App() {
-  const embed  = () =>{
+  const embed = () => {
     return {
-      __html : '<spline-viewer loading-anim-type="spinner-big-light" url="https://prod.spline.design/oQF2wFKmvwQjdZjp/scene.splinecode"></spline-viewer>'
+      __html: '<spline-viewer loading-anim-type="spinner-big-light" url="https://prod.spline.design/oQF2wFKmvwQjdZjp/scene.splinecode"></spline-viewer>'
     }
   }
   return (
     <>
-        <div className="App">
-    <div className="mobileView" dangerouslySetInnerHTML={embed()} style={{
-                 width: "99vw",
-                 height: "99vh" }}>
-            </div>
-         <iframe className="laptopView" title="laptopView" src='https://my.spline.design/portfoliolandscapecopy-9c51313ccd2976e0f1764142768a8f5d/' frameBorder='0' width='100vw' height='100vh' style={{
-                 width: "99vw",
-                 height: "99vh" }} ></iframe>
-
-          {/* <ComingSoon /> */}
-         
+      <div className="App">
+        <div dangerouslySetInnerHTML={embed()} style={{
+          width: "99vw",
+          height: "99vh"
+        }}>
         </div>
+        {/* <iframe className="laptopView" title="laptopView" src='https://my.spline.design/portfoliolandscapecopy-9c51313ccd2976e0f1764142768a8f5d/' frameBorder='0' width='100vw' height='100vh' style={{
+                 width: "99vw",
+                 height: "99vh" }} ></iframe> */}
+
+        {/* <ComingSoon /> */}
+
+      </div>
     </>
   );
 }
